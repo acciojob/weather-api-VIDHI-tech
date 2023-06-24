077e81ad9f3b3d289fb1c3fa751ccf3945584ce5
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-24 14:10:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 function getCurrentWeather() {
-  // Replace YOUR_API_KEY with your actual API key obtained from OpenWeatherMap
-  const apiKey = 'https://openweathermap.org/api';
+  const apiKey = 'YOUR_API_KEY';
   const city = 'London';
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-  // Make an HTTP request to the OpenWeatherMap API
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-      // Extract the weather description from the API response
       const weatherDescription = data.weather[0].description;
-
-      // Update the weatherData div with the weather information
       const weatherDataDiv = document.getElementById('weatherData');
       weatherDataDiv.textContent = `Current weather in ${city}: ${weatherDescription}`;
     })
