1641e1fc2d6aede6db46160d5ae162d884c63551
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-24 14:19:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-function getCurrentWeather() {
-  const apiKey = 'https://openweathermap.org/api';
+async function getCurrentWeather() {
+  const apiKey = 'YOUR_API_KEY';
   const city = 'London';
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-  fetch(apiUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to fetch weather data');
-      }
-      return response.json();
-    })
-    .then(data => {
-      const weatherDescription = data.weather[0].description;
-      const weatherDataDiv = document.getElementById('weatherData');
-      weatherDataDiv.textContent = `Current weather in ${city}: ${weatherDescription}`;
-    })
-    .catch(error => {
-      console.log('An error occurred while fetching the weather data:', error);
-    });
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error('Failed to fetch weather data');
+    }
+    const data = await response.json();
+    const weatherDescription = data.weather[0].description;
+    const weatherDataDiv = document.getElementById('weatherData');
+    weatherDataDiv.textContent = `Current weather in ${city}: ${weatherDescription}`;
+  } catch (error) {
+    console.log('An error occurred while fetching the weather data:', error);
+  }
 }
